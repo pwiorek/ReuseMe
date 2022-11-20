776c7fde758f3ec832f869fb25ce08c1824e3e75
pwiorek <piotr.wiorek71@gmail.com>
2022-11-20 11:31:26 +0100

    feat: last adjustments

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from "@angular/material/dialog";
 import { FiltersComponent } from "@core/components/filters/filters.component";
+import { ActivatedRoute, NavigationEnd, Router } from "@angular/router";
+import { filter } from "rxjs";
 
 @Component({
   selector: 'app-navbar',
@@ -10,12 +12,22 @@ import { FiltersComponent } from "@core/components/filters/filters.component";
 export class NavbarComponent implements OnInit {
   showArrowBackIcon: boolean = false;
   showFilterIcon: boolean = true;
+  showBookmarkIcon: boolean = false;
 
   constructor(
-    private dialog: MatDialog
+    private dialog: MatDialog,
+    private route: ActivatedRoute,
+    private router: Router
   ) { }
 
   ngOnInit(): void {
+    this.router.events.pipe(
+      filter(val => val instanceof NavigationEnd)
+    ).subscribe(val => {
+      this.showArrowBackIcon = this.router.url != '/';
+      this.showFilterIcon = this.router.url == '/';
+      this.showBookmarkIcon = this.router.url != '/' && !this.router.url.includes('create');
+    })
   }
 
   public showFilters(): void {
@@ -24,4 +36,5 @@ export class NavbarComponent implements OnInit {
       height: '50vh'
     });
   }
+
 }
